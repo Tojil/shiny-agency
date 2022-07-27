7c069c7e7e54e09263c4bb919d65ab4906223605
Sergio Canto Sabido <sergio@hanullimparis.com>
2022-07-27 12:16:56 +0200

    test

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,17 +3,3 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
-import { render, screen } from '@testing-library/react'
-import React from 'react'
-import ReactDOM from 'react'
-import App from './App'
-
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-})
-
-it('renders welcome message', () => {
-  render(<App />)
-  expect(screen.getByText('learn React')).toBeInTheDocument()
-})
